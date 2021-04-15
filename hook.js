38baa2d7d564ce817bfd3fe200761501fdb31881
Michael <82292036+schumannlive@users.noreply.github.com>
2021-04-15 09:03:44 +0200

    Update hook.js

--- hook.js
+++ hook.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js')
 
 const webhook = new Discord.WebhookClient('first section of the link - paste here!','second section of the link - paste here!') ///Provide your webhook url identificators
 
-var sch1 = ('http://sosrff.tsu.ru/new/shm.jpg ')
-var sch2 = ('http://sosrff.tsu.ru/new/sra.jpg ')
-var sch3 = ('http://sosrff.tsu.ru/new/srf.jpg ')
-var sch4 = ('http://sosrff.tsu.ru/new/srq.jpg ')
-var sch5 = ('http://www.vlf.it/cumiana/last_E-VLF.jpg ')
-var sch6 = ('http://www.vlf.it/cumiana/last-geomar.jpg ')
-var sch7 = ('http://www.vlf.it/cumiana/last-plotted.jpg ')
+var sch1 = ('http://sosrff.tsu.ru/new/shm.jpg ')	/// define graph links
+var sch2 = ('http://sosrff.tsu.ru/new/sra.jpg ')	/// define graph links
+var sch3 = ('http://sosrff.tsu.ru/new/srf.jpg ')	/// define graph links
+var sch4 = ('http://sosrff.tsu.ru/new/srq.jpg ')	/// define graph links
+var sch5 = ('http://www.vlf.it/cumiana/last_E-VLF.jpg ')	/// define graph links
+var sch6 = ('http://www.vlf.it/cumiana/last-geomar.jpg ')	/// define graph links
+var sch7 = ('http://www.vlf.it/cumiana/last-plotted.jpg ')	/// define graph links
 
 var date = new Date();
 
-const embed = new Discord.MessageEmbed()
+const embed = new Discord.MessageEmbed()	/// define embed message sent after the graphs (modify as you wish)
     	.setColor('0xf7f7f8')
     	.setTitle('Schumannlive')
 	.setURL('https://twitter.com/schumannlive')
@@ -26,9 +26,9 @@ const embed = new Discord.MessageEmbed()
 
     
 
-setInterval(schPost, 1000*60*120)
+setInterval(schPost, 1000*60*120)	/// define time interval (1000 milliseconds times 60 (1 minute) times 120 - 2 hours)
 
-    function schPost(){    
+    function schPost(){    	/// send webhook messages (graphs and embed) in previously defined time interval
         webhook.send(sch1)
         webhook.send(sch2)
         webhook.send(sch3)
