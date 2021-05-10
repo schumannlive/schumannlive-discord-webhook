b4c617eec748e52f0a7939eb513636cb3d439cf5
Michael <82292036+schumannlive@users.noreply.github.com>
2021-05-10 19:24:49 +0200

    Update hookRomagna.js

--- hookRomagna.js
+++ hookRomagna.js
@@ -6,19 +6,19 @@ setInterval(Romagna, 1000*60*120)
 
 function Romagna() {
 
-  var date = new Date();
-
-  const romagnaEmbed = new Discord.MessageEmbed()
-  .attachFiles(["./media/test.jpg", "./media/last-borehole-sogliano.jpg"])
-  .setColor('#ffffff')
+	var date = new Date();
+	
+	const romagnaEmbed = new Discord.MessageEmbed()
+	.attachFiles(["./media/test.jpg", "./media/last-borehole-sogliano.jpg"])
+	.setColor('#ffffff')
 	.setTitle(date.toUTCString())
 	.setURL('https://twitter.com/schumannlive')
 	.setThumbnail('https://pbs.twimg.com/profile_images/1378114668414308356/0u_ykowi_400x400.jpg')
 	.addFields(
-    { name: 'Romagna, Italy', value: 'http://www.uevlf.it/uevlf.HTML', inline: false },
-  )
+	{ name: 'Romagna, Italy', value: 'http://www.uevlf.it/uevlf.HTML', inline: false },
+	)
 
-  webhook.send(romagnaEmbed)
+	webhook.send(romagnaEmbed)
 
 }
 
