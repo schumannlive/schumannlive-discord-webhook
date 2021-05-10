8e165b51177dcbb4a3dbac61b4dcbd6c3eaa00ee
Michael <82292036+schumannlive@users.noreply.github.com>
2021-05-10 19:25:31 +0200

    Update hookCumiana.js

--- hookCumiana.js
+++ hookCumiana.js
@@ -7,19 +7,19 @@ setInterval(Cumiana, 1000*60*120)
 
 function Cumiana() {
 
-  var date = new Date();
+	var date = new Date();
 
-  const cumianaEmbed = new Discord.MessageEmbed()
-  .attachFiles(["./media/last-geomar.jpg", "./media/last_E-VLF.jpg", "./media/last-plotted.jpg", "./media/last-marconi-multistrip-slow.jpg"])
-  .setColor('#ffffff')
+	const cumianaEmbed = new Discord.MessageEmbed()
+	.attachFiles(["./media/last-geomar.jpg", "./media/last_E-VLF.jpg", "./media/last-plotted.jpg", "./media/last-marconi-multistrip-slow.jpg"])
+	.setColor('#ffffff')
 	.setTitle(date.toUTCString())
 	.setURL('https://twitter.com/schumannlive')
 	.setThumbnail('https://pbs.twimg.com/profile_images/1378114668414308356/0u_ykowi_400x400.jpg')
 	.addFields(
-    { name: 'Cumiana, Italy', value: 'http://www.vlf.it/cumiana/livedata.html', inline: false },
-  )
+	{ name: 'Cumiana, Italy', value: 'http://www.vlf.it/cumiana/livedata.html', inline: false },
+	)
 
-  webhook.send(cumianaEmbed)
+	webhook.send(cumianaEmbed)
 
 }
 
