1b11b594c3a22916701b080af870b69d342b8fdc
Michael <82292036+schumannlive@users.noreply.github.com>
2021-05-10 19:23:56 +0200

    Update hookRussia.js

--- hookRussia.js
+++ hookRussia.js
@@ -6,19 +6,19 @@ setInterval(Russia, 1000*60*120)
 
 function Russia() {
 
-  var date = new Date();
+	var date = new Date();
 
-  const russiaEmbed = new Discord.MessageEmbed()
-  .attachFiles(["./media/shm.jpg", "./media/sra.jpg", "./media/srf.jpg", "./media/srq.jpg"])
-  .setColor('#ffffff')
+	const russiaEmbed = new Discord.MessageEmbed()
+	.attachFiles(["./media/shm.jpg", "./media/sra.jpg", "./media/srf.jpg", "./media/srq.jpg"])
+	.setColor('#ffffff')
 	.setTitle(date.toUTCString())
 	.setURL('https://twitter.com/schumannlive')
 	.setThumbnail('https://pbs.twimg.com/profile_images/1378114668414308356/0u_ykowi_400x400.jpg')
 	.addFields(
-    { name: 'Tomsk, Russia', value: 'http://sosrff.tsu.ru/', inline: false },
-  )
+	{ name: 'Tomsk, Russia', value: 'http://sosrff.tsu.ru/', inline: false },
+	)
 
-  webhook.send(russiaEmbed)
+  	webhook.send(russiaEmbed)
 
 }
 
